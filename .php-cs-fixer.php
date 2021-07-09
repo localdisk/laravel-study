0a8dbae15b015a69f2582bd1a41342d254379e13
localdisk <smoochynet@gmail.com>
2021-07-09 21:09:08 +0900

    remove comment.

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,24 +1,5 @@
 <?php declare(strict_types=1);
 
-// $finder = PhpCsFixer\Finder::create()
-    // ->in([
-    //     __DIR__ . '/app',
-    //     __DIR__ . '/config',
-    //     __DIR__ . '/database/factories',
-    //     __DIR__ . '/database/seeders',
-    //     __DIR__ . '/routes',
-    //     __DIR__ . '/tests',
-    // ]);
-    // ->in([
-    //     '/config',
-    //     '/app',
-    //     '/database/factories',
-    //     '/database/seeders',
-
-    //     '/routes',
-    //     '/tests',
-    // ]);
-
 $config = new PhpCsFixer\Config();
 
 return $config
@@ -50,4 +31,3 @@ return $config
         'array_syntax' => ['syntax' => 'short'],
     ]
     );
-    // ->setFinder($finder);
